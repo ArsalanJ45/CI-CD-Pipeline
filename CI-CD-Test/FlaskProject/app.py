1acaf36f2a76f93abf1a3b98973974380d1b69c8
Arsalan Jahid <arsalanjahid123@gmail.com>
2024-11-15 01:37:41 +0500

    pipeline

---
+++ CI-CD-Test/FlaskProject/app.py
@@ -0,0 +1,9 @@
+python 
+Copy code 
+from flask import Flask 
+app = Flask(__name__) 
+@app.route('/') 
+def home(): 
+return "Hello, CI/CD!" 
+if __name__ == '__main__': 
+app.run()
---
+++ CI-CD-Test/FlaskProject/test_app.py
@@ -0,0 +1,11 @@
+python 
+Copy code 
+from app import app 
+ 
+def test_home(): 
+    with app.test_client() as client: 
+        response = client.get('/') 
+        assert response.status_code == 200 
+        assert response.data == b"Hello, CI/CD!"
+
+        
